d02c04fad920285b47b5974146549cdf1b5df823
Gaurav Pal <spare.slant@gmail.com>
2021-11-14 20:22:51 +0000

    multi-stack working code

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python
-from constructs import Construct
-from cdktf import App, TerraformStack, TerraformOutput
-from imports.oci import (
-    OciProvider,
-    IdentityCompartment
-    )
-from account import tenancy_profile_name, get_local_oci_config_value
+from cdktf import App
 
-class MyStack(TerraformStack):
-    def __init__(self, scope: Construct, ns: str):
-        super().__init__(scope, ns)
-
-        # define resources here
-        tenancyID = get_local_oci_config_value(tenancy_profile_name, "tenancy")
-        OciProvider(self, "oci",
-                config_file_profile=tenancy_profile_name)
-        
-        comp = IdentityCompartment(self, "compartment_cdk",
-                name="CDK",
-                description="CDK compartment",
-                compartment_id=tenancyID)
-
-        TerraformOutput(self, "cdk_compartment_id",
-                value=comp.id)
-
+from privileged_oci import PrivilegedUser
+from network_oci import Network
 
 app = App()
-MyStack(app, "create_privileged_user")
+PrivilegedUser(app, "create_privileged_user")
+Network(app, "create_network")
 
 app.synth()
